4104a46cfd18758fc9618ed4d4012e91f0646bdd
Brett Sykes <bsykes@akamai.com>
2016-12-08 15:40:54 -0500

    Use SIGWINCH to detect screen size change.

--- lib/smooth_terminal_print.rb
+++ lib/smooth_terminal_print.rb
@@ -2,56 +2,47 @@ require 'stringio'
 require_relative './terminal_actions'
 
 module SmoothTerminalPrint
-    include TerminalActions
-    extend self
-    trap('SIGINT') { SmoothTerminalPrint.stop_stp; exit }
-    at_exit        { SmoothTerminalPrint.stop_stp }
+	include TerminalActions
+	extend self
+	trap('SIGINT')   { SmoothTerminalPrint.stop_stp; exit }
+	trap('SIGWINCH') { get_screen_dimensions }
+	at_exit          { SmoothTerminalPrint.stop_stp }
 
-    def start_stp(io)
-        get_screen_dimensions
-        print(hide_cursor)
-        print(move_to_top_left)
+	def start_stp(io)
+		get_screen_dimensions
+		print(hide_cursor)
+		print(move_to_top_left)
 
-        print_text(io)
-    end
+		print_text(io)
+	end
 
-    def stop_stp
-        print(move_to_bottom)
-        print(show_cursor)
-    end
+	def stop_stp
+		print(move_to_bottom)
+		print(show_cursor)
+	end
 
-    private
-    def print_text(io)
-        io.rewind
+	def get_screen_dimensions
+		@num_lines = `tput lines`.strip.to_i - 2
+		@columns   = `tput cols`.strip.to_i - 5
 
-        0.upto(@num_lines) do
-            line = io.gets
+		print clear_screen
+	end
 
-            if line.nil?
-                line = ''
-            else
-                line.strip!
-            end
+	private
+	def print_text(io)
+		io.rewind
 
-            line << clear_to_line_end
-            puts line
-        end
-    end
+		0.upto(@num_lines) do
+			line = io.gets
 
-    def get_screen_dimensions
-        @stp_reset_timer ||= nil
+			if line.nil?
+				line = ''
+			else
+				line.strip!
+			end
 
-        if(@stp_reset_timer == nil || Time.now.to_i - @stp_reset_timer > 3)
-            old_num_lines   = @num_lines
-            old_num_cols    = @columns
-            @num_lines      = (`tput lines`).strip.to_i - 2
-            @columns        = `tput cols`.strip.to_i - 5
-
-            if(old_num_lines != @num_lines || old_num_cols != @columns)
-                clear_screen
-            end
-
-            @stp_reset_timer = Time.now.to_i
-        end
-    end
+			line << clear_to_line_end
+			puts line
+		end
+	end
 end
